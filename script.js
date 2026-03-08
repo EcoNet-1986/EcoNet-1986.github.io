b252c14260e6027296baa1753337fd8f27d6629a
EcoNet-1986 <leithoussama02@gmail.com>
2026-03-08 15:47:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -16,37 +16,46 @@ const provider = new GoogleAuthProvider();
 
 let myId = "", myData = null, currentPath = "posts-public", selectedContactId = null;
 
-// --- CONNEXION AUTOMATIQUE ---
 onAuthStateChanged(auth, async (user) => {
     if (user) {
         const snap = await get(child(ref(db), `utilisateurs/${user.uid}`));
+
         if (snap.exists()) {
             myId = user.uid;
             myData = snap.val();
             startApp();
+
         } else {
-        // Premier passage : Choix du rôle
-        const rolesValides = ["eleve", "parent", "professeur", "directeur"];
-        let roleChoisi = "";
-        
-        while(!rolesValides.includes(roleChoisi)){
-            roleChoisi = prompt("Quel est ton rôle ? (eleve, parent, professeur, directeur)");
-            
-            if(roleChoisi){
-                roleChoisi = roleChoisi.toLowerCase().trim();
+
+            // Premier passage : Choix du rôle
+            const rolesValides = ["eleve", "parent", "professeur", "directeur"];
+            let roleChoisi = "";
+
+            while(!rolesValides.includes(roleChoisi)){
+                roleChoisi = prompt("Quel est ton rôle ? (eleve, parent, professeur, directeur)");
+
+                if(roleChoisi){
+                    roleChoisi = roleChoisi.toLowerCase().trim();
+                }
+
+                if(!rolesValides.includes(roleChoisi)){
+                    alert("Veuillez écrire seulement : eleve, parent, professeur ou directeur.");
+                }
             }
-    
-            if(!rolesValides.includes(roleChoisi)){
-                alert("Veuillez écrire seulement : eleve, parent, professeur ou directeur.");
-            }
+
+            myData = {
+                nom: user.displayName,
+                role: roleChoisi,
+                enLigne: true,
+                email: user.email
+            };
+
+            await set(ref(db, `utilisateurs/${user.uid}`), myData);
+
+            myId = user.uid;
+            startApp();
         }
-    
-        myData = { nom: user.displayName, role: roleChoisi, enLigne: true, email: user.email };
-    
-        await set(ref(db, `utilisateurs/${user.uid}`), myData);
-    
-        myId = user.uid;
-        startApp();
+
     } else {
         document.getElementById('screen-login').style.display = 'block';
         document.getElementById('screen-app').style.display = 'none';
